03d8a18312f2c7fda116034754cea7194e2f5b86
Bao Nguyen <nguyenlegiabaoab@gmail.com>
2023-11-28 10:16:38 +0000

    custom in server dev

---
+++ src/app/lib/getLogWork.js
@@ -0,0 +1,20 @@
+
+export default async function getLogWork() {
+  const response = await fetch('http://api-jira.lotustest.net/rest/V1/user/thao',{
+   				method: 'GET',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': 'http://192.168.11.153:3001',
+										'Access-Control-Allow-Methods':'*',
+										'Access-Control-Allow-Credentials':'true',
+										'Access-Control-Allow-Headers':'X-CSRF-Token'
+                },
+  })
+
+  if(!response.ok) {
+     throw new Error('failed to fetch users')
+  }
+
+   return await response.json()
+}
