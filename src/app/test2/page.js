555779e00740cfdb3b903bd24553d424ae21b874
Bao Nguyen <nguyenlegiabaoab@gmail.com>
2023-12-01 10:32:35 +0000

    update api

---
+++ src/app/test2/page.js
@@ -0,0 +1,47 @@
+
+
+import LogWorksPage2 from '../test2/uitest2'
+import { auth } from '@/app/auth'
+
+const PageLog = async () => {
+
+  const { user } = await auth()
+  const getUserIssue = async () => {
+    'use server'
+    const arr=[]
+    const totalTimeLive = await
+    fetch(`http://api-jira.lotustest.net/rest/V1/user/${user.username}`,
+      {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods':'*',
+          'Access-Control-Allow-Credentials':'true',
+          'Access-Control-Allow-Headers':'X-CSRF-Token'
+
+
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        data.map((item) => (
+          arr.push(item)
+        ))
+
+      })
+
+    return arr
+  }
+
+  const dataIssue = await getUserIssue()
+  return (
+    <>
+    <LogWorksPage2 dataIssue = {dataIssue}></LogWorksPage2>
+    </>
+ 
+  )
+}
+
+export default PageLog
