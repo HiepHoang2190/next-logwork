5cd28286674b2093525a3caffd80c0a58759f198
Minh Tran <minht@lotusoutsourcing.com>
2024-01-26 10:41:46 +0700

    fix bugs

--- src/app/lib/fetchApi.js
+++ src/app/lib/fetchApi.js
@@ -39,25 +39,17 @@ export const fetchWithCredentials = async (url, options = {}) => {
   }
 };
 
-const authHeaders = async () => {
+export const getUserIssues = async (username, year, month, lastDayOfMonth) => {
+  const url = `${process.env.JIRA_API_PATH}/api/2/search?jql=(worklogAuthor%20in%20(%22${username}%22))%20AND%20(worklogDate%20%3E%3D%20%27${year}-${month}-01%27%20and%20worklogDate%20%3C%20%27${year}-${month}-${lastDayOfMonth}%27)%20&fields=summary%2Cworklog%2Ccreated%2Cupdated%2Cissuetype%2Cparent%2Cproject%2Cstatus%2Cassignee%2Creporter%2Caggregatetimespent%2Ctimeoriginalestimate%2Ctimeestimate&maxResults=1000`;
   const { user } = await auth();
-  return {
+  const header = {
     method: 'GET',
     headers: {
       'Cookie': `JSESSIONID=${user.session.value}`,
     },
   };
-};
-
-const fetchDataWithCredentials = async (url) => {
-  const headers = await authHeaders();
-  return fetchWithCredentials(url, headers);
-};
-
-export const getUserIssues = async (username, year, month, lastDayOfMonth) => {
-  const url = `${process.env.JIRA_API_PATH}/api/2/search?jql=(worklogAuthor%20in%20(%22${username}%22))%20AND%20(worklogDate%20%3E%3D%20%27${year}-${month}-01%27%20and%20worklogDate%20%3C%20%27${year}-${month}-${lastDayOfMonth}%27)%20&fields=summary%2Cworklog%2Ccreated%2Cupdated%2Cissuetype%2Cparent%2Cproject%2Cstatus%2Cassignee%2Creporter%2Caggregatetimespent%2Ctimeoriginalestimate%2Ctimeestimate&maxResults=1000`;
   try {
-    const data = await fetchDataWithCredentials(url);
+    const data = await fetchWithCredentials(url, header);
     const arr = []
 
     data.issues.map((item) => (
@@ -66,39 +58,60 @@ export const getUserIssues = async (username, year, month, lastDayOfMonth) => {
     return arr
 
   } catch (error) {
-   return (error.message);
+    return (error.message);
   }
 };
 
 export const getUserCurrentIssues = async () => {
   const url = `${process.env.JIRA_API_PATH}/api/2/search?jql=assignee%3DcurrentUser()%20AND%20resolution%3DUnresolved%20and%20status%20!%3D%20Closed&fields=issuetype%2Csummary%2Creporter%2Cpriority%2Cstatus%2Cresolution%2Ccreated%2Cupdated&maxResults=1000`
+  const { user } = await auth();
+  const header = {
+    method: 'GET',
+    headers: {
+      'Cookie': `JSESSIONID=${user.session.value}`,
+    },
+  };
   try {
-    const data = await fetchDataWithCredentials(url);
+    const data = await fetchWithCredentials(url, header);
     const arr = []
-    
+
     data.issues.map((item) => (
       arr.push(item)
     ));
     return arr
-  
+
   } catch (error) {
-   return (error.message);
+    return (error.message);
   }
 }
 
 export const getWorklogCurrentIssue = async (issueKey) => {
   const url = `${process.env.JIRA_API_PATH}/api/2/issue/${issueKey}/worklog?maxResults=5000`
+  const { user } = await auth();
+  const header = {
+    method: 'GET',
+    headers: {
+      'Cookie': `JSESSIONID=${user.session.value}`,
+    },
+  };
   try {
-    const data = await fetchDataWithCredentials(url);
+    const data = await fetchWithCredentials(url, header);
     return data
   } catch (error) {
-   return (error.message);
+    return (error.message);
   }
 }
 
 export const getAllDataUser = async () => {
   const url = `${process.env.API_PATH}/V1/all-user`;
-  const data = await fetchDataWithCredentials(url);
+  const { user } = await auth();
+  const header = {
+    method: 'GET',
+    headers: {
+      'Cookie': `JSESSIONID=${user.session.value}`,
+    },
+  };
+  const data = await fetchWithCredentials(url, header);
   const arr = []
 
   data.map((item) => (
@@ -110,7 +123,14 @@ export const getAllDataUser = async () => {
 
 export const getTimeLeaveTotal = async (username) => {
   const url = `${process.env.API_PATH}/V1/timeleave/${username}`
-  const data = await fetchDataWithCredentials(url);
+  const { user } = await auth();
+  const header = {
+    method: 'GET',
+    headers: {
+      'Cookie': `JSESSIONID=${user.session.value}`,
+    },
+  };
+  const data = await fetchWithCredentials(url, header);
   const arr = []
 
   data.map((item) => (
@@ -122,7 +142,14 @@ export const getTimeLeaveTotal = async (username) => {
 
 export const getTimeLeave = async (username) => {
   const url = `${process.env.API_PATH}/V1/leave/${username}`
-  const data = await fetchDataWithCredentials(url);
+  const { user } = await auth();
+  const header = {
+    method: 'GET',
+    headers: {
+      'Cookie': `JSESSIONID=${user.session.value}`,
+    },
+  };
+  const data = await fetchWithCredentials(url, header);
   const arr = []
 
   data.map((item) => (
