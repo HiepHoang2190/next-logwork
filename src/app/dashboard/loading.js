cafc1dea4ea3bff2a58425124ea40c0a036a08fc
Minh Tran <minht@lotusoutsourcing.com>
2024-12-26 22:13:53 +0700

    fix package

--- src/app/dashboard/loading.js
+++ src/app/dashboard/loading.js
@@ -4,7 +4,7 @@ import 'react-spinner-animated/dist/index.css'
 const Loading = () => {
 
   return (
-    <div style={{ "position": "absolute", "top": "45%", "left": "55%", "marginTop": 0, "marginLeft": 0, "borderRadius": 0.5 + "rem;" }}>
+    <div style={{ "position": "absolute", "top": "45%", "left": "55%", "marginTop": 0, "marginLeft": 0, "borderRadius": "0.5rem" }}>
       <HalfMalf center={false} text={"Loading..."} />
     </div>
   );
